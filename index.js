27a8f9f9760ab9e8530f659f0615106178d1bb8b
Ramiro Yaben <r.yaben@viseven.com>
2021-11-08 21:33:32 -0300

    update(index.js): imported ejs layouts and placed route loadings

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
+//Declaraciones
 const express = require('express');
-var controller = require('./server/controller');
+const controller = require('./server/controller');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 5000;
 
 app.use(express.static(__dirname));
+app.use(expressLayouts);
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
-    controller.loadDictionary();
+//Sets del express-ejs-layouts
+app.set('layout', './layouts/normal');
+app.set('view engine', 'ejs');
+
+//Rutas
+app.get('/diccionario', (req, res) => {
+    res.render('dictionary', { title: 'Balanlàedenuges ljngeg'})
+});
+app.get('/listado-palabras', (req, res) => {
+    res.render('listing', { title: 'Balanlàedenuges ljngeg'})
+});
+app.get('/sustantivos', (req, res) => {
+    res.render('nouns', { title: 'Balanlàedenuges ljngeg'})
+});
+app.get('/verbos', (req, res) => {
+    res.render('verbs', { title: 'Balanlàedenuges ljngeg'})
+});
+app.get('/adjetivos', (req, res) => {
+    res.render('adjectives', { title: 'Balanlàedenuges ljngeg'})
+});
+app.get('/adverbios', (req, res) => {
+    res.render('adverbs', { title: 'Balanlàedenuges ljngeg'})
+});
+app.get('/pronombres', (req, res) => {
+    res.render('articles', { title: 'Balanlàedenuges ljngeg'})
+});
+app.get('/adposiciones', (req, res) => {
+    res.render('adpositions', { title: 'Balanlàedenuges ljngeg'})
+});
+app.get('/composiciones', (req, res) => {
+    res.render('compounds', { title: 'Balanlàedenuges ljngeg'})
+});
+app.get('/sintaxis', (req, res) => {
+    res.render('syntaxis', { title: 'Balanlàedenuges ljngeg'})
 });
 
 app.listen(port, () => {
