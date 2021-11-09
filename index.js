623f3a03fdbe6afd86f50a0e24520ee848efb345
Ramiro Yaben <ramiroyaben@gmail.com>
2021-11-09 01:12:33 -0300

    update(index.js): added page titles dynamically

--- index.js
+++ index.js
@@ -14,34 +14,34 @@ app.set('view engine', 'ejs');
 
 //Rutas
 app.get('/diccionario', (req, res) => {
-    res.render('dictionary', { title: 'Balanlàedenuges ljngeg'})
+    res.render('dictionary', { title: 'Balanlàedenuges ljngeg - Diccionario'});
 });
-app.get('/listado-palabras', (req, res) => {
-    res.render('listing', { title: 'Balanlàedenuges ljngeg'})
+app.get('/listado', (req, res) => {
+    res.render('listing', { title: 'Balanlàedenuges ljngeg - Listado de palabras'})
 });
 app.get('/sustantivos', (req, res) => {
-    res.render('nouns', { title: 'Balanlàedenuges ljngeg'})
+    res.render('nouns', { title: 'Balanlàedenuges ljngeg - Sustantivos'})
 });
 app.get('/verbos', (req, res) => {
-    res.render('verbs', { title: 'Balanlàedenuges ljngeg'})
+    res.render('verbs', { title: 'Balanlàedenuges ljngeg - Verbos'})
 });
 app.get('/adjetivos', (req, res) => {
-    res.render('adjectives', { title: 'Balanlàedenuges ljngeg'})
+    res.render('adjectives', { title: 'Balanlàedenuges ljngeg - Adjetivos'})
 });
 app.get('/adverbios', (req, res) => {
-    res.render('adverbs', { title: 'Balanlàedenuges ljngeg'})
+    res.render('adverbs', { title: 'Balanlàedenuges ljngeg - Adverbios'})
 });
 app.get('/pronombres', (req, res) => {
-    res.render('articles', { title: 'Balanlàedenuges ljngeg'})
+    res.render('articles', { title: 'Balanlàedenuges ljngeg - Pronombres y artículos'})
 });
 app.get('/adposiciones', (req, res) => {
-    res.render('adpositions', { title: 'Balanlàedenuges ljngeg'})
+    res.render('adpositions', { title: 'Balanlàedenuges ljngeg - Adposiciones'})
 });
 app.get('/composiciones', (req, res) => {
-    res.render('compounds', { title: 'Balanlàedenuges ljngeg'})
+    res.render('compounds', { title: 'Balanlàedenuges ljngeg - Composiciones y ampliaciones'})
 });
 app.get('/sintaxis', (req, res) => {
-    res.render('syntaxis', { title: 'Balanlàedenuges ljngeg'})
+    res.render('syntaxis', { title: 'Balanlàedenuges ljngeg - Sintaxis y ortografía'})
 });
 
 app.listen(port, () => {
