f89b7244899c5ffcd301d268b5672d691742eb6a
Ramiro Yaben <ryaben@viseven.com>
2022-01-08 14:38:24 -0300

    update(controller.js, mysql.js): created a function to avoid app crashing on PROTOCOL_CONNECTION_LOST with MySQL server on Heroku, and migrated connection establishment to controller

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,28 @@
-const connection = require('./mysql');
+var mysql = require('mysql');
+const db_config = require('./mysql');
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 3000);
+    }
+  });
+
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 const query = 
     `SELECT 
--- server/mysql.js
+++ server/mysql.js
@@ -1,12 +1,9 @@
-var mysql = require('mysql');
-
-var connection = mysql.createConnection({
+var db_config = {
   host: 'us-cdbr-east-05.cleardb.net',
   port: '3306',
   user: 'b0dd19ff59d876',
   password: '48dd8f8ab07676e',
-  database: 'heroku_bf7cb810553a372',
-  multipleStatements: true
-});
+  database: 'heroku_bf7cb810553a372'
+};
 
-module.exports = connection;
+module.exports = db_config;
